79e46492c2a86ea2f440b8de37c135183c07e46d
Gelo <geloadrian24@gmail.com>
2023-02-10 10:52:27 +0800

    wut the hell

--- Github Testing/Program.cs
+++ Github Testing/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 
 namespace MyApp 
+
   // Note: actual namespace depends on the project name.
 { // change one
   // testing number two
   // final testing
+
     internal class Program
     {
         static void Main(string[] args)
