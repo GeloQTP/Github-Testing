73458d6ba9606d357f475491ed3f2c1e7433c5b0
Gelo <geloadrian24@gmail.com>
2023-02-10 10:20:47 +0800

    Changes number 2

--- Github Testing/Program.cs
+++ Github Testing/Program.cs
@@ -2,6 +2,7 @@
 
 namespace MyApp // Note: actual namespace depends on the project name.
 { // change one
+    // testing number two
     internal class Program
     {
         static void Main(string[] args)
