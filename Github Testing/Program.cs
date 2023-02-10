122b07e2a8004b42ed3817c443754d71e989745f
Gelo <geloadrian24@gmail.com>
2023-02-10 18:44:49 +0800

    1

--- Github Testing/Program.cs
+++ Github Testing/Program.cs
@@ -4,15 +4,13 @@ namespace MyApp
 
   // Note: actual namespace depends on the project name.
 { // change one
-  // testing number two
-  // final testing
-  // wut the heeeeeeeeeeeeelllllllllllllllll
 
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Goodbye World");
         }
     }
 }
