e3836d76ab4532f2c76c413037b43d601f484cc2
Gelo <geloadrian24@gmail.com>
2023-02-10 10:21:17 +0800

    final testing

--- Github Testing/Program.cs
+++ Github Testing/Program.cs
@@ -3,6 +3,7 @@
 namespace MyApp // Note: actual namespace depends on the project name.
 { // change one
     // testing number two
+    // final testing
     internal class Program
     {
         static void Main(string[] args)
