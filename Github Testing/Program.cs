0c96105fdf93cb641fd94a420d259f2ada9e000c
Gelo <geloadrian24@gmail.com>
2023-02-14 18:56:15 +0800

    Final Change

--- Github Testing/Program.cs
+++ Github Testing/Program.cs
@@ -1,11 +1,30 @@
 ﻿int score; 
 
-Console.WriteLine("Please Enter your First Number: ");
+Console.WriteLine("Please Enter your First Score: ");
 score = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Please Enter your First Number: ");
+Console.WriteLine("Please Enter your Second Score: ");
 score += Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Please Enter your First Number: ");
+Console.WriteLine("Please Enter your Third Score: ");
 score += Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Please Enter your Fourth Score: ");
+score += Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please Enter your Fifth Score: ");
+score += Convert.ToInt32(Console.ReadLine());
+
+var total_score = score / 5;
+
+Console.WriteLine("\n");
+
+if (total_score >= 75)
+{
+    Console.WriteLine("You Passed!");
+}
+
+else
+{
+    Console.WriteLine("You Failed!");
+}
